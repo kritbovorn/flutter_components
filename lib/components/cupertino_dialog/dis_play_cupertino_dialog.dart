0a743846b97e171dfdafde92e77ecb2fed69235c
kritbovorn <ktaweeyossak@yahoo.com>
2022-07-13 18:18:47 +0700

    add CupertinoDialog()

---
+++ lib/components/cupertino_dialog/dis_play_cupertino_dialog.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+
+class DisPlayCupertinoDialog extends StatelessWidget {
+  final String title;
+  final String content;
+  final VoidCallback onPressed;
+  const DisPlayCupertinoDialog({
+    Key? key,
+    required this.title,
+    required this.content,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoAlertDialog(
+      title: Text(title),
+      content:  Text(content),
+      actions: [
+        CupertinoDialogAction(
+          onPressed: onPressed,
+          child: const Text('Ok'),
+        ),
+      ],
+    );
+  }
+}
