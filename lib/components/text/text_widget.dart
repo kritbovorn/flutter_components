0c9febc8ddf071dacb76f6b4e1585711a8941144
kritbovorn <ktaweeyossak@yahoo.com>
2022-07-25 20:09:58 +0700

    add TextWidget()

---
+++ lib/components/text/text_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class TextWidget extends StatelessWidget {
+  final String title;
+  final double? height;
+  final Color? titleColor;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final num? padding;
+  const TextWidget({
+    Key? key,
+    required this.title,
+    this.height,
+    this.titleColor,
+    this.fontSize,
+    this.fontWeight,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          // var pw = constraints.maxWidth * (padding ?? 0.02);
+          double font = constraints.maxHeight;
+          return FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Text(
+              title,
+              style: TextStyle(
+                color: titleColor ?? Colors.white,
+                fontSize: font,
+                fontWeight: fontWeight,
+                height: 1,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
